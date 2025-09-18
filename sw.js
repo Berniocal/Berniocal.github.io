8b359f2f291d91ca76d74a7edbb34572cada55df
Bernio <jendabernard@gmail.com>
2025-09-18 19:58:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_STATIC  = 'calc-static-v7';
-const CACHE_RUNTIME = 'calc-runtime-v7';
+const CACHE_STATIC  = 'calc-static-v8';
+const CACHE_RUNTIME = 'calc-runtime-v8';
 
 const SCOPE = self.registration.scope;
 const PRECACHE = [
@@ -58,4 +58,5 @@ self.addEventListener('fetch', (e) => {
     })());
   }
 });
+
 
