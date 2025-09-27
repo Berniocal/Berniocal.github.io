d2651dc6287a454aaf1cbbb840c9e161723a4f65
Bernio <jendabernard@gmail.com>
2025-09-27 06:56:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Bernio PWA Service Worker
 // verzi zvyšuj při každém nasazení (stejně jako SW_VERSION v index.html)
-const VERSION = '2025-09-29b';
+const VERSION = '2025-09-30b';
 const APP_CACHE = `bernio-app-${VERSION}`;
 const RUNTIME_CACHE = `bernio-runtime-${VERSION}`;
 
@@ -117,5 +117,6 @@ self.addEventListener('fetch', (event) => {
     return (await c.match(req)) || fetch(req);
   })());
 });
+
 
 
